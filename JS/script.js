18bea921e22dcb4bf922ca3c91c349b4c2ded147
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-11-04 11:48:31 +0100

    update display contact on click

--- JS/script.js
+++ JS/script.js
@@ -12,17 +12,17 @@ let app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Hai portato a spasso il cane?',
-                        /* status: 'sent' */
+                        status: 'sent'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Ricordati di dargli da mangiare',
-                        /* status: 'sent' */
+                        status: 'sent'
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         text: 'Tutto fatto!',
-                        /* status: 'received' */
+                        status: 'received'
                     },
                 ]
             },
@@ -34,17 +34,17 @@ let app = new Vue({
                     {
                         date: '20/03/2020 16:30:00',
                         text: 'Ciao come stai?',
-                        /* status: 'sent' */
+                        status: 'sent'
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         text: 'Bene grazie! Stasera ci vediamo?',
-                        /* status: 'received' */
+                        status: 'received'
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        /* status: 'sent' */
+                        status: 'sent'
                     }
                 ]
             },
@@ -56,17 +56,17 @@ let app = new Vue({
                     {
                         date: '28/03/2020 10:10:40',
                         text: 'La Marianna va in campagna',
-                        /* status: 'received' */
+                        status: 'received'
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         text: 'Sicuro di non aver sbagliato chat?',
-                        /* status: 'sent' */
+                        status: 'sent'
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         text: 'Ah scusa!',
-                        /* status: 'received' */
+                        status: 'received'
                     }
                 ],
             },
@@ -78,16 +78,75 @@ let app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Lo sai che ha aperto una nuova pizzeria?',
-                        /* status: 'sent' */
+                        status: 'sent'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Si, ma preferirei andare al cinema',
-                        /* status: 'received' */
+                        status: 'received'
                     }
                 ],
             },
-        ]
+        ],
+        currentChat:
+        {
+            name: 'Michele',
+            avatar: 'IMG/avatar_1.jpg',
+            /* visible: true, */
+            messages: [
+                {
+                    date: '10/01/2020 15:30:55',
+                    text: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 15:50:00',
+                    text: 'Ricordati di dargli da mangiare',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 16:15:22',
+                    text: 'Tutto fatto!',
+                    status: 'received'
+                },
+            ]
+        }
     },
-    methods: {}
+    methods: {
+        getLastMessage(messages) {
+            if (messages.length === 0) {
+                return "";
+            }
+            let lastMessage = messages[messages.length - 1].text;
+            return lastMessage.slice(0, 20);
+        },
+        getLastTimeStamp(messages) {
+            if (messages.length === 0) {
+                return "";
+            }
+            let lastTimeStamp = messages[messages.length - 1].date;
+            return lastTimeStamp;
+        },
+
+
+        activeChat(contact) {
+            this.currentChat = contact;
+        },
+        timeChat() {
+            let chatOnDisplay = this.currentChat;
+            if (chatOnDisplay.messages.length === 0) {
+                return "";
+            }
+            let thisTime = chatOnDisplay.messages[chatOnDisplay.messages.length - 1].date;
+            return thisTime;
+        },
+
+
+
+        autoResponse() {
+            setTimeout(() => {
+                //codice
+            }, 1000);
+        }
+    }
 });
